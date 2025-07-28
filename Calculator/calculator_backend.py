0ec3658b4fdceeaae848a3d2d4bd17690ab0d88a
Prakhar Agrawal <130597671+code3reaper@users.noreply.github.com>
2025-07-28 21:45:22 +0530

    Delete Calculator directory

--- Calculator/calculator_backend.py
+++
@@ -1,8 +0,0 @@
-# calculator_backend.py
-
-def evaluate_expression(expression):
-    try:
-        result = eval(expression)
-    except Exception:
-        raise ValueError("Invalid expression")
-    return result
